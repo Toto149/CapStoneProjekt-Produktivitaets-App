0532c5c45b92ba0612be5229460ac071cb1b112b
Thorsten Thomann <thorsten.thomann@googlemail.com>
2024-01-15 17:03:30 +0100

    add App.tsx and a Calendar with an Calendar library

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,54 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Calendar from "react-calendar";
+import TodoCard from "./components/TodoCard.tsx";
+import {Todo} from "./model/Todo.ts";
+
+type ValuePiece = Date | null;
+
+type Value = ValuePiece | [ValuePiece, ValuePiece];
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [value, setValue] = useState<Value>(new Date());
 
+    const listOfTodos: Todo[]= [
+        {
+            id: "1",
+            title: "test",
+            description: "test",
+            startDate: new Date("2023-01-02"),
+            deadline: new Date("2024-01-17")
+
+
+        }
+    ];
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Calendar onChange={setValue} value={value} />
+        {
+            listOfTodos.map(
+                todo => {
+                    if(!(value) || "getDate" in value
+                    && todo.startDate.getDate() <= value.getDate()
+                    && todo.deadline.getDate() >= value.getDate())
+                    {
+                        return (   <TodoCard
+                                    key={todo.id}
+                                    id={todo.id}
+                                    title={todo.title}
+                                    description={todo.description}
+                                    startDate={todo.startDate}
+                                    deadline={todo.deadline}
+                                />
+                            );
+                    }
+                }
+            )
+        }
+
+    </div>
   )
 }
 
