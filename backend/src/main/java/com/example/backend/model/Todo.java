d91b7790153c41abe9c92586421960df80afb9dc
Thorsten Thomann <thorsten.thomann@googlemail.com>
2024-01-15 16:57:46 +0100

    add startDate to Todo class

--- backend/src/main/java/com/example/backend/model/Todo.java
+++ backend/src/main/java/com/example/backend/model/Todo.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 public record Todo (String id,
                     String title,
                     String description,
+                    LocalDateTime startDate,
                     LocalDateTime deadline,
                     Importance gradeOfImportance,
                     CompletionTime timeToComplete) {
