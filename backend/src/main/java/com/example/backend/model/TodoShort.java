0187e35e7adf46971774527c0ada3c69219874e1
Thorsten Thomann <thorsten.thomann@googlemail.com>
2024-01-15 17:06:48 +0100

    add title and startDate to TodoShort

--- backend/src/main/java/com/example/backend/model/TodoShort.java
+++ backend/src/main/java/com/example/backend/model/TodoShort.java
@@ -3,6 +3,8 @@ package com.example.backend.model;
 import java.time.LocalDateTime;
 
 public record TodoShort(String id,
+                        String title,
                         String description,
+                        LocalDateTime startDate,
                         LocalDateTime deadline) {
 }
