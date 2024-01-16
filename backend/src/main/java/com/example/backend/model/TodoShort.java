54d0680efb2c603f4e3359fd76bad1ee4219c138
Thorsten Thomann <thorsten.thomann@googlemail.com>
2024-01-16 16:38:24 +0100

    add Document annotation

--- backend/src/main/java/com/example/backend/model/TodoShort.java
+++ backend/src/main/java/com/example/backend/model/TodoShort.java
@@ -1,10 +1,15 @@
 package com.example.backend.model;
 
-import java.time.LocalDateTime;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDateTime;
+@Document(collection = "todoShort")
 public record TodoShort(String id,
                         String title,
                         String description,
+                        @JsonFormat(pattern = "YYYY-MM-DD hh:mm:ss")
                         LocalDateTime startDate,
+                        @JsonFormat(pattern = "YYYY-MM-DD hh:mm:ss")
                         LocalDateTime deadline) {
 }
