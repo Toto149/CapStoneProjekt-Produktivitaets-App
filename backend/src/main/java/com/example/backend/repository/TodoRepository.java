46d3f758a6792d83a11e61b080060cfe02668899
Thorsten Thomann <thorsten.thomann@googlemail.com>
2024-01-16 16:36:14 +0100

    change use of Todo to TodoShort

--- backend/src/main/java/com/example/backend/repository/TodoRepository.java
+++ backend/src/main/java/com/example/backend/repository/TodoRepository.java
@@ -1,10 +1,10 @@
 package com.example.backend.repository;
 
-import com.example.backend.model.Todo;
+import com.example.backend.model.TodoShort;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TodoRepository extends MongoRepository<Todo, String> {
+public interface TodoRepository extends MongoRepository<TodoShort, String> {
 
 }
