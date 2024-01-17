db626cd588e87450df06fb03a700ca099632fcf6
Thorsten Thomann <thorsten.thomann@googlemail.com>
2024-01-17 18:14:02 +0100

    not really important

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy :{
-      '/todo' : {
-        target: 'http://localhost:8080'
+      "/todo" : {
+        target: "http://localhost:8080"
       }
     }
   }
