41c056629f28d12f291e4509fc849669666f8215
Thorsten Thomann <thorsten.thomann@googlemail.com>
2024-01-17 18:13:43 +0100

    change Repo Dataclass to new Dataclass

--- backend/src/main/java/com/example/backend/repository/TodoRepository.java
+++ backend/src/main/java/com/example/backend/repository/TodoRepository.java
@@ -1,10 +1,10 @@
 package com.example.backend.repository;
 
-import com.example.backend.model.TodoShort;
+import com.example.backend.model.TodoShortInternalDTO;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TodoRepository extends MongoRepository<TodoShort, String> {
+public interface TodoRepository extends MongoRepository<TodoShortInternalDTO, String> {
 
 }
