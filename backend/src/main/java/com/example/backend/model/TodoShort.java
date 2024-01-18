d53c46c10489e3b3f29ef685c951e8b2c84f7368
Thorsten Thomann <thorsten.thomann@googlemail.com>
2024-01-18 09:49:13 +0100

    remove @Document, since this is no longer used for the MongoDB

--- backend/src/main/java/com/example/backend/model/TodoShort.java
+++ backend/src/main/java/com/example/backend/model/TodoShort.java
@@ -1,9 +1,6 @@
 package com.example.backend.model;
-
-import org.springframework.data.mongodb.core.mapping.Document;
-
 import java.time.LocalDateTime;
-@Document(collection = "todoShort")
+
 public record TodoShort(String id,
                         String title,
                         String description,
