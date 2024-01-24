ad9b5dc9008294b9a0c7d11d745ee274ebf5899d
Thorsten Thomann <thorsten.thomann@googlemail.com>
2024-01-24 11:47:49 +0100

    Add put, post and delete

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Calendar from "react-calendar";
 import TodoCard from "./components/TodoCard.tsx";
-import {Todo} from "./model/Todo.ts";
+import { Todo } from "./model/Todo.ts";
 import axios from "axios";
+import TodoForm from "./components/TodoForm.tsx";
+import './Calendar.css'
+
 
 type ValuePiece = Date | null;
 
@@ -13,17 +16,56 @@ type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 function App() {
     const [value, setValue] = useState<Value>(new Date());
-    const [todos, setTodos] = useState<Todo[]>([{
-        id: "1",
-        title: "test",
-        description: "testetstets",
-        startDate: new Date(),
-        deadline: new Date()
+    const [todos, setTodos] = useState<Todo[]>([])
 
-    }])
+    const [titleValue, setTitleValue] = useState<string>("");
+    const [description, setDescription ] = useState<string>("");
+    const [startDate, setStartDate] = useState<Date>(new Date());
+    const [deadline, setDeadline] = useState<Date>(new Date());
 
-    const fetchTodos = () => {
-        axios.get("/todo")
+    const [titleValueEdit, setTitleValueEdit] = useState<string>("");
+    const [descriptionEdit, setDescriptionEdit ] = useState<string>("");
+    const [startDateEdit, setStartDateEdit] = useState<Date>(new Date());
+    const [deadlineEdit, setDeadlineEdit] = useState<Date>(new Date());
+
+    function formatToLocalDateTimeString(date: Date): string {
+        return date.toISOString().slice(0,16);
+
+    }
+    const handleSubmit = () => {
+        axios.post("/api/todo/save",
+            {
+                title: titleValue,
+                description: description,
+                startDate: formatToLocalDateTimeString(startDate),
+                deadline: formatToLocalDateTimeString(deadline)
+            })
+            .then(response => setTodos([...todos, response.data]))
+            .catch(error => console.error("Error posting Data: ", error)
+            );
+
+    };
+    const handleUpdate = (id:string) => {
+        axios.put("/api/todo/save/" + id,
+            {
+                title:titleValueEdit,
+                description: descriptionEdit,
+                startDate: formatToLocalDateTimeString(startDateEdit),
+                deadline: formatToLocalDateTimeString(deadlineEdit)
+            })
+            .then(response => response.status === 200 ? alert("Update Successful") : alert("Update was unsuccessful"))
+            .catch(error => console.error("Error updating Data: ", error)
+            );
+    }
+    const handleDelete = (id:string) => {
+        axios.delete("/api/todo/delete/" + id)
+            .then(response => {if(response.data){
+                alert("Todo removed succesfully!")
+            }})
+            .catch(err => alert(err.message));
+    };
+    const fetchTodos =  () => {
+        axios.get("/api/todo/get")
             .then(response => {
                 const todos = response.data.map(
                      (todo :Todo) => {
@@ -41,8 +83,8 @@ function App() {
     };
 
     useEffect(() => {
-        fetchTodos();
-    }, [setValue]);
+            fetchTodos();
+    }, [todos]);
 
 
 
@@ -65,12 +107,33 @@ function App() {
                                     description={todo.description}
                                     startDate={todo.startDate}
                                     deadline={todo.deadline}
+                                    delete={handleDelete}
+                                    submitHandler={handleUpdate}
+                                    setTitle={setTitleValueEdit}
+                                    setDes={setDescriptionEdit}
+                                    setStart={setStartDateEdit}
+                                    setDateDeadline={setDeadlineEdit}
+                                    titleEdit={titleValueEdit}
+                                    descriptionEdit={descriptionEdit}
+                                    startDateEdit={startDateEdit}
+                                    deadlineEdit={deadlineEdit}
+
                                 />
                             );
                     }
                 }
             )
     }
+    <TodoForm title={titleValue}
+              description={description}
+              startDate={startDate}
+              deadline={deadline}
+              submitHandler={handleSubmit}
+              setTitle={setTitleValue}
+              setDes={setDescription}
+              setStart={setStartDate}
+              setDateDeadline={setDeadline}
+    />
     </div>
   )
 }
