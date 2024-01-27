9b54177c601457d75f76cfc6a5d32209145dc96e
Thorsten Thomann <thorsten.thomann@googlemail.com>
2024-01-27 20:08:02 +0100

    change Service to work for new Dataclasses add transformStringToCompletionTime add transformIntGradeOfImportanceToString for Data handling the new Dataclasses

--- backend/src/main/java/com/example/backend/service/TodoService.java
+++ backend/src/main/java/com/example/backend/service/TodoService.java
@@ -1,15 +1,15 @@
 package com.example.backend.service;
 
-import com.example.backend.model.PostTodo;
-import com.example.backend.model.TodoShort;
-import com.example.backend.model.TodoShortDB;
+import com.example.backend.model.*;
 import com.example.backend.repository.TodoRepository;
+import com.example.backend.model.Importance;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Service
 @RequiredArgsConstructor
@@ -17,9 +17,9 @@ public class TodoService {
     private final TodoRepository repo;
     private final IdService idService;
     DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
-    public List<TodoShort> getAllTodos() {
-        List<TodoShortDB> todos = repo.findAll();
-        return transformTodoShortDBToTodoShort(todos);
+    public List<Todo> getAllTodos() {
+        List<TodoDB> todos = repo.findAll();
+        return transformTodoDBToTodo(todos);
 
     }
 
@@ -40,27 +40,75 @@ public class TodoService {
 
         ).toList();
     }
+    private String transformIntGradeOfImportanceToString(int gradeOfImportance){
 
-    public TodoShort saveTodo(PostTodo todoToSave) {
-        TodoShortDB todoSavedToDB = repo.save(new TodoShortDB(
+        switch (gradeOfImportance){
+            case 1:
+                return "BARELY_IMPORTANT";
+            case 2:
+                return "MODERATELY_IMPORTANT";
+            case 3:
+                return "IMPORTANT";
+            case 4:
+                return "QUITE_IMPORTANT";
+            case 5:
+                return "VERY_IMPORTANT";
+            default:
+                throw new IllegalArgumentException("Must be a integer between 1 and 5 inclusively");
+
+
+        }
+    }
+    public Todo saveTodo(PostTodo todoToSave) {
+        TodoDB todoSavedToDB = repo.save(
+                new TodoDB(
                 idService.generateId(),
                 todoToSave.title(),
                 todoToSave.description(),
                 todoToSave.startDate().formatted(dateTimeFormatter),
-                todoToSave.deadline().formatted(dateTimeFormatter)
+                todoToSave.deadline().formatted(dateTimeFormatter),
+                transformIntGradeOfImportanceToString(todoToSave.gradeOfImportance()),
+                todoToSave.timeToComplete()
         ));
-        List<TodoShortDB> list = List.of(todoSavedToDB);
-        return transformTodoShortDBToTodoShort(list).get(0);
+        List<TodoDB> list = List.of(todoSavedToDB);
+        return transformTodoDBToTodo(list).get(0);
     }
 
-    public TodoShort updateTodo(String id, PostTodo todoToUpdate) {
-        repo.deleteById(id);
-        TodoShortDB todoToUpdateDB = repo.save(new TodoShortDB(id,
+    public Todo updateTodo(String id, PostTodo todoToUpdate) {
+
+        TodoDB todoToUpdateDB = repo.save(new TodoDB(
+                id,
                 todoToUpdate.title(),
                 todoToUpdate.description(),
                 todoToUpdate.startDate().formatted(dateTimeFormatter),
-                todoToUpdate.deadline().formatted(dateTimeFormatter)));
-        List<TodoShortDB> list = List.of(todoToUpdateDB);
-        return transformTodoShortDBToTodoShort(list).get(0);
+                todoToUpdate.deadline().formatted(dateTimeFormatter),
+                transformIntGradeOfImportanceToString(todoToUpdate.gradeOfImportance()),
+                todoToUpdate.timeToComplete()
+                ));
+        List<TodoDB> list = List.of(todoToUpdateDB);
+        return transformTodoDBToTodo(list).get(0);
     }
+    public static CompletionTime transformStringToCompletionTime(String timeString){
+        //The String contains of the int Value and the unit like "100 hours", "100 minutes" etc
+        String[] timeAndUnitInArray = timeString.split(" ");
+        int amount = Integer.parseInt(timeAndUnitInArray[0]);
+        TimeUnit unit = TimeUnit.valueOf(timeAndUnitInArray[1].toUpperCase());
+        return new CompletionTime(amount,unit);
+
+    }
+    public static List<Todo> transformTodoDBToTodo(List<TodoDB> todos){
+        return todos.stream().map(
+                todoInter -> new Todo(
+                        todoInter.id(),
+                        todoInter.title(),
+                        todoInter.description(),
+                        LocalDateTime.parse(todoInter.startDate(), DateTimeFormatter.ISO_LOCAL_DATE_TIME),
+                        LocalDateTime.parse(todoInter.deadline(), DateTimeFormatter.ISO_LOCAL_DATE_TIME),
+                        Importance.valueOf(todoInter.gradeOfImportance()),
+                        transformStringToCompletionTime(todoInter.timeToComplete())
+                )
+                ).toList();
+    }
+
+
 }
