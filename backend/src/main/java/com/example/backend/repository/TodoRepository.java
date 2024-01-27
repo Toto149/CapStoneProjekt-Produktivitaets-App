1ccd76267fecb9edcd108cb7a9aab9794d1c2f09
Thorsten Thomann <thorsten.thomann@googlemail.com>
2024-01-27 20:06:34 +0100

    change repo to new class

--- backend/src/main/java/com/example/backend/repository/TodoRepository.java
+++ backend/src/main/java/com/example/backend/repository/TodoRepository.java
@@ -1,10 +1,11 @@
 package com.example.backend.repository;
 
-import com.example.backend.model.TodoShortDB;
+
+import com.example.backend.model.TodoDB;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TodoRepository extends MongoRepository<TodoShortDB, String> {
+public interface TodoRepository extends MongoRepository<TodoDB, String> {
 
 }
