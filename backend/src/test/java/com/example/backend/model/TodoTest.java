42c7febe36fc8a66840790d30a43936ce44f4b8b
Thorsten Thomann <thorsten.thomann@googlemail.com>
2024-02-05 11:17:08 +0100

    change Todo.java and add some Test for it

--- backend/src/test/java/com/example/backend/model/TodoTest.java
+++ backend/src/test/java/com/example/backend/model/TodoTest.java
@@ -65,4 +65,32 @@ class TodoTest {
         //THEN
         assertEquals(expected,actual);
     }
+    @Test
+    void testCompareTo_shouldReturnMinus1_whenElementsComparedAreEqualApartFromTimeToComplete() {
+        //GIVEN
+        Todo td1 = new Todo(
+                "1",
+                "Test",
+                "Test",
+                LocalDateTime.of(2023,12,12,12,12,12),
+                LocalDateTime.of(2024,1,22,2,2,2),
+                Importance.MODERATELY_IMPORTANT,
+                new CompletionTime(9, TimeUnit.HOURS)
+
+        );
+        Todo td2 = new Todo(
+                "2",
+                "Test",
+                "Test",
+                LocalDateTime.of(2023,12,12,12,12,12),
+                LocalDateTime.of(2024,1,22,2,2,2),
+                Importance.MODERATELY_IMPORTANT,
+                new CompletionTime(10,TimeUnit.HOURS)
+        );
+        int expected = -1;
+        //WHEN
+        int actual = td1.compareTo(td2);
+        //THEN
+        assertEquals(expected,actual);
+    }
 }
